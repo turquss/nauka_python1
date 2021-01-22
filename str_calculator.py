1609d488fcef95fdf3b1869669e8da01f7e164c1
turquss <turquss@users.noreply.github.com>
2021-01-22 21:59:32 +0100

    kalkulator dla str

---
+++ str_calculator.py
@@ -0,0 +1,9 @@
+def str_calculator(a, b, operacja):
+    if operacja == 'concat':
+        return a + b
+   if operacja == "contain":
+       pass
+#    if operacja == "*":
+#        return a * b
+#    if operacja == "/":
+#        return a / b
