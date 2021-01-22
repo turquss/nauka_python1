a8cacbd8ac381b98df1f44da9f010ddc5394bfa4
turquss <turquss@users.noreply.github.com>
2021-01-22 21:28:37 +0100

    kalkulator

---
+++ kalkulator.py
@@ -0,0 +1,9 @@
+def calculate(a, b, operacja):
+    if operacja == "+":
+        return a + b
+    if operacja == "-":
+        return a - b
+    if operacja == "*":
+        return a * b
+    if operacja == "/":
+        return a / b
